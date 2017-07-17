23a5b713f4a826361afe9db0d76322a4577ce3a9
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-07-17 15:08:44 -0600

    rename hello-angular component to angular

--- app/javascript/angular/app/app.component.ts
+++ app/javascript/angular/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hello-angular',
+  selector: 'angular',
   template: `<h1>Hello {{name}}</h1>`
 })
+
 export class AppComponent {
   name = 'Angular!';
 }
