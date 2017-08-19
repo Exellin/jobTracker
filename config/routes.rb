8812693aba4ad2a5cc32031c8c9f89bd5dac009f
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-08-19 17:05:11 -0600

    Add jobs_controller with create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root 'pages#home'
   get '*unmatched_route', to: 'pages#home'
+  resources :jobs, only: [:create]
 end
