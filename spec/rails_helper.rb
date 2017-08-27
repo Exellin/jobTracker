47c14fe070cdd5e04b5859d0822e90f6dd7b8817
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-08-27 01:26:46 -0600

    Configure webpacker to run in the test environment before rspec
    Stale javascript can lead to tests passing with errors within angular

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    system 'RAILS_ENV=test ./bin/rails webpacker:compile'
+  end
 end
