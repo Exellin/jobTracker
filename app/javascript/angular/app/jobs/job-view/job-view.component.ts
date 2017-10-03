1e6281e8f754adeb39b6024b74b0872c0dae1bfb
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-10-03 17:22:42 -0600

    Add ngIf directive on shown dates in order for the date pipe to only apply on valid dates
    This is so browsers without a date html element won't give an error for the applied pipe
    The ngIf directive evaluates a function returning an observable with an asychronous pipe

--- app/javascript/angular/app/jobs/job-view/job-view.component.ts
+++ app/javascript/angular/app/jobs/job-view/job-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import templateString from './job-view.component.html';
 
@@ -12,5 +13,16 @@ export class JobViewComponent implements OnInit {
   constructor() {}
 
   public ngOnInit(): void {
+  }
+
+  private validDate(dateString: string): Observable<any> {
+    let result: boolean;
+    const dateObject: Date = new Date(dateString);
+    if (!isNaN(dateObject.getDate())) {
+      result = true;
+    } else {
+      result = false;
+    }
+    return Observable.of(result);
   }
 }
